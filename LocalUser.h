2feb72cee7487826b914f07948ac281ad47d9b5a
Raya Asgari <asgariraya@gmail.com>
2025-12-28 20:08:24 +0330

    fixing

--- LocalUser.h
+++ LocalUser.h
@@ -6,31 +6,31 @@
 
 class LocalUser : public User
 {
-vector<string> conversations;
+vector<string> conversation;
 public:
-  LocalUser(long int userId, string userName, string name, int userBirthday, string userPassword, int loginData)
+  LocalUser(long int userId, string userName, string name, string userBirthday, string userPassword, int loginData)
 : User(userId, userName, name, userBirthday, userPassword, loginData) {}
 
 void showDashboard() const
 {
     cout << "Welcome to our offline messenger, " << getUserName() << ":)" << endl;
-    cout << "1. Show Users\n2. Show Conversations\n3. Create Private Chat\n4. Logout\n";
+    cout << "1. Show Users\n2. Show Conversations\n3. Create Private Chat\n4. Logout"<<endl;
 }
 
 void showConversations() const
 {
     cout << "Your conversations:" << endl;
-    for (const auto& conv : conversations) 
+    for (const auto& conv : conversation) 
     {
         cout << " " << conv << endl;
     }
 }
  void addConversation(const string& name) 
     {
-        conversations.push_back(name);
+        conversation.push_back(name);
     }
     void logout() 
     {
-        cout << "User logged out.\n";
+        cout << "User logged out."<<endl;
     }
 };
