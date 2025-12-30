f006354f14b951b95b8f289e4b71ae61714567c5
Raya Asgari <asgariraya@gmail.com>
2025-12-30 21:04:14 +0330

    last add

--- VoiceMessage.cpp
+++ VoiceMessage.cpp
@@ -3,19 +3,17 @@
 
 using namespace std;
 
-VoiceMessage::VoiceMessage(int senderId, const string& time, const string& username): Message(senderId, time), senderUsername(username) {}
+VoiceMessage::VoiceMessage(int senderId, const string& time, const string& desc) : Message(senderId, time) {}
 
-void VoiceMessage::print() const 
-{
+void VoiceMessage::print() const {
     cout << "[Voice Message] Sent by user ID: " << senderId
          << " at " << sendTime << endl;
 }
 
-std::string VoiceMessage::toString() const 
+string VoiceMessage::toString() const 
 {
-    return "[Voice Message] Sent by " + senderUsername + " at " + sendTime;
+    return "[Voice Message] Sent by user ID: " + to_string(senderId) +  " at " + sendTime;
 }
-
 
 string VoiceMessage::getType() const {
     return "Voice";
